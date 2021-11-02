0549a41110a741d258c37a080f98e7ad35848998
SpongeF <34061883+SpongeF@users.noreply.github.com>
2021-11-02 20:55:15 +0800

    Update decision.py

--- decision.py
+++ decision.py
@@ -2,44 +2,10 @@ import os
 import torch as T
 import torch.nn.functional as F
 import torch.nn as nn
-import numpy as np
 # from networks import ActorNetwork, CriticNetwork,  ValueNetwork
 import torch.optim as optim
 from torch.distributions.normal import Normal
 
-class ReplayBuffer():
-    def __init__(self, max_size, input_shape, n_actions):
-        self.mem_size = max_size
-        self.mem_cntr = 0
-        self.state_memory = np.zeros((self.mem_size, *input_shape))
-        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
-        self.action_memory = np.zeros((self.mem_size, n_actions))
-        self.reward_memory = np.zeros(self.mem_size)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
-
-    def store_transition(self, state, action, reward, state_, done):
-        index = self.mem_cntr % self.mem_size
-
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        self.action_memory[index] = action
-        self.reward_memory[index] = reward
-        self.terminal_memory[index] = done
-
-        self.mem_cntr += 1
-
-    def sample_buffer(self, batch_size):
-        max_mem = min(self.mem_cntr, self.mem_size)
-
-        batch = np.random.choice(max_mem, batch_size)
-
-        states = self.state_memory[batch]
-        states_ = self.new_state_memory[batch]
-        actions = self.action_memory[batch]
-        rewards = self.reward_memory[batch]
-        dones = self.terminal_memory[batch]
-
-        return states, actions, rewards, states_, dones
 
 class Agent():
     def __init__(self, alpha=0.0003, beta=0.0003, input_dims=[8],
